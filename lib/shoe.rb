3961f936104af1252981217940681bd9faa5f787
James Scalise <jamescscalise@gmail.com>
2019-08-27 23:22:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -15,7 +15,7 @@ class Shoe
   
   def brand=(brand)
     @brand = brand
-    if !(brand.in(BRANDS))
+    if !(BRANDS.includes(brand))
       @brand = brand
       BRANDS << brand
       puts "adding " + brand
